a6c12eab65a267ca6a526a2edab2dc37885804cc
hc182 <khch503@naver.com>
2024-02-27 15:13:20 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/posts">Post Link</Link>
+                </li>
+                <li>
+                    <Link to="/posts/:id">Post Detail</Link>
+                </li>
+                <li>
+                    <Link to="/posts/edit/:id">Post Edit</Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route path="/" element={<h1>안녕하세요</h1>} />
+                <Route path="/posts" element={<h1>Post List Page</h1>} />
+                <Route path="/posts/:id" element={<h1>Post List Page</h1>} />
+                <Route
+                    path="/posts/edit/:id"
+                    element={<h1>Post List Page</h1>}
+                />
+                <Route path="profile" element={<h1>프로필 페이지</h1>}></Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
